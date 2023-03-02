69cd8a81688dcbdf130d24de8239eac065602510
foxgeun <121280503+foxgeun@users.noreply.github.com>
2023-03-02 21:25:56 +0900

    Summary

---
+++ escape9_hint/script.js
@@ -0,0 +1,64 @@
+
+var input = document.getElementsByTagName('input');
+var login = document.getElementById('log-in');
+var code = document.getElementById('code');
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var interval = setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+      }
+      if(timer === 0) {
+        clearInterval(interval);
+        display.textContent = "시간종료!";
+      }
+    }, 1000);
+  }
+  
+  window.onload = function () {
+    /* 기본값 10(분)입니다. */
+    var minutes = 60;
+  
+    var fiveMinutes = (60 * minutes) - 1,
+      display = document.querySelector('#MyTimer');
+    startTimer(fiveMinutes, display);
+  };
+  
+login.onclick = ()=>{
+
+    if(code.value == "1h13"){
+        window.open('result.html', 'window_name', 'width=430, height=500, location=no, status=no, scrollbars=yes')
+        return false; 
+    }
+    
+    else if(code.value == "1r25"){
+      window.open('result3.html', 'window_name', 'width=430, height=500, location=no, status=no, scrollbars=yes')
+      return false;  
+    }
+    else if(code.value == "1s27"){
+      window.open('result4.html', 'window_name', 'width=430, height=500, location=no, status=no, scrollbars=yes')
+      return false;  
+    }
+    else if(code.value == "1j38"){
+      window.open('result5.html', 'window_name', 'width=430, height=500, location=no, status=no, scrollbars=yes')
+      return false;  
+    }
+    else if(code.value == "1k34"){
+      window.open('result6.html', 'window_name', 'width=430, height=500, location=no, status=no, scrollbars=yes')
+      return false;  
+    }
+    
+    else{
+        alert("힌트코드를 다시 입력해주세요.")
+        return false; 
+    }
+}
